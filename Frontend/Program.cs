e1ce2d91c5435ccaa57d4e45a039706dfd60d5a0
Akshat Singh <akshats20@iiserb.ac.in>
2025-12-28 02:26:09 +0000

    Created a frontend project using Silk.NET

---
+++ Frontend/Program.cs
@@ -0,0 +1,57 @@
+﻿using Silk.NET.Input;
+using Silk.NET.Maths;
+using Silk.NET.Windowing;
+
+namespace Frontend
+{
+    public static class Program
+    {
+        private static IWindow _window;
+        public static void Main(string[] args)
+        {
+            WindowOptions options = WindowOptions.Default with
+            {
+                Size = new Vector2D<int>(800, 600),
+                Title = "My first Silk.NET application!"
+            };    
+
+            _window = Window.Create(options);
+            
+            _window.Load += OnLoad;
+            _window.Update += OnUpdate;
+            _window.Render += OnRender;
+
+            _window.Run();
+
+            
+        }
+
+        private static void OnLoad()
+        {
+            Console.WriteLine("Load!");
+
+            IInputContext input = _window.CreateInput();
+
+            for (int i = 0; i < input.Keyboards.Count; i++)
+                input.Keyboards[i].KeyDown += KeyDown;
+        }
+
+        private static void OnUpdate(double deltaTime)
+        {
+            
+        }
+
+        private static void OnRender(double deltaTime)
+        {
+            
+        }
+
+        private static void KeyDown(IKeyboard keyboard, Key key, int keyCode)
+        {
+            if (key == Key.Escape)
+                _window.Close();
+        }
+
+        
+    }
+}

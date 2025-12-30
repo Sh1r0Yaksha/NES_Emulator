30c99eaac1408be178dd85e6a4fad724edb6200e
Akshat Singh <akshats20@iiserb.ac.in>
2025-12-30 00:47:53 +0000

    Added Input Enum

---
+++ NES/Input.cs
@@ -0,0 +1,14 @@
+namespace NES
+{
+    public enum Input
+{
+    A      = 1 << 0,
+    B      = 1 << 1,
+    Select = 1 << 2,
+    Start  = 1 << 3,
+    Up     = 1 << 4,
+    Down   = 1 << 5,
+    Left   = 1 << 6,
+    Right  = 1 << 7,
+}
+}
